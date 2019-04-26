ec30efb50c6df91e75298b31a19bda999da0fa9e
Rory Taca <rorytaca@gmail.com>
2019-04-26 15:23:23 -0400

    add: core component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import React from "react";
+
+class BackgroundVideo extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      lazyLoaded: null,
+    }
+  }
+
+  componentDidMount() {
+    //load video buffer
+
+    //fade out image
+
+    //fade in video
+  }
+
+  render() {
+    const { posterSrc, posterAlt, videoSrc } = this.props;
+
+    return (
+      <React.Fragment>
+        <img src={posterSrc} alt={posterSrc} />
+        <video loop autoPlay muted>
+          { videoSrc.map(video => <source src={video.src} type={video.mediaType} />)}
+        </video>
+      </React.Fragment>
+    )
+  }
+};
+
+export default BackgroundVideo;
